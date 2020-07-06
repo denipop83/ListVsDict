08e6633700dc682e52d6d1ab788a9ca7886cee95
Попов Денис <dpopov@stream.ru>
2020-07-06 12:38:35 +0500

    Add project files.

---
+++ ListVsDict/ListVsDict.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace ListVsDict
+{
+	[RPlotExporter]
+	public class ListVsDict
+	{
+		[Params(50, 100)]
+		public int N;
+
+		private KindaComplexObject[] data;
+		List<KindaComplexObject> list1;
+		Dictionary<int, KindaComplexObject> dict1;
+
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			data = new KindaComplexObject[N];
+			list1 = new List<KindaComplexObject>(N);
+			dict1 = new Dictionary<int, KindaComplexObject>(N);
+
+			for (var i = 0; i < N; i++)
+			{
+				var kindaComplexObject = KindaComplexObject.Create(i);	
+				data[i] = kindaComplexObject;
+			}
+		}
+
+		[Benchmark]
+		public void FillAndSeekList()
+		{
+			for (var i = 0; i < N; i++)
+				list1.Add(data[i]);
+
+			foreach (var obj in data)
+			{
+				var seek = list1.FirstOrDefault(i => i.Id == obj.Id);
+			}
+		}
+
+		[Benchmark]
+		public void FillAndSeekDict()
+		{
+			dict1 = data.ToDictionary(d => d.Id);
+
+			foreach (var obj in data)
+			{
+				var seek = dict1[obj.Id];
+			}
+		}
+
+
+		[Benchmark]
+		public void FillKnownSizeList()
+		{
+			var list = new List<KindaComplexObject>(N);
+		
+			foreach (var kindaComplexObject in data)
+				list.Add(kindaComplexObject);
+		}
+		
+		[Benchmark]
+		public void FillKnownSizeDict()
+		{
+			var hashset = new HashSet<KindaComplexObject>(N);
+			foreach (var kindaComplexObject in data)
+				hashset.Add(kindaComplexObject);
+		}
+	}
+
+	public class KindaComplexObject
+	{
+		public int Id { get; private set; }
+		public int Rating { get; private set; }
+		public string Description { get; private set; }
+		public InternalInfo Info { get; private set; }
+
+		public class InternalInfo
+		{
+			public string MoreDescription { get; internal set; }
+			public int InternalId { get; internal set; }
+		}
+
+		private KindaComplexObject(){}
+
+		public static KindaComplexObject Create(int id) =>
+			new KindaComplexObject()
+			{
+				Description = $"desc_{id}",
+				Id = id,
+				Rating = id,
+				Info = new InternalInfo()
+				{
+					InternalId = id,
+					MoreDescription = $"more_{id}"
+				}
+			};
+
+		public override int GetHashCode() => Id;
+	}
+}
---
+++ ListVsDict/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Running;
+
+namespace ListVsDict
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<ListVsDict>();
+
+			//var s = new ListVsDict();
+			//s.Setup();
+			//s.FillAndSeekDict();
+
+			//var dict = new Dictionary<int, KindaComplexObject>(3);
+			//var dict2 = new HashSet<KindaComplexObject>(3);
+			//var data = new KindaComplexObject[3];
+			//
+			//for (var i = 0; i < 3; i++)
+			//	data[i] = KindaComplexObject.Create(i);
+			//
+			//for (int i = 0; i < 3; i++)
+			//	dict.Add(i, data[i]);
+			//
+			//for (int i = 0; i < 3; i++)
+			//	dict2.Add(data[i]);
+		}
+	}
+}

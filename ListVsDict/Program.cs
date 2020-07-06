1fe48260dd5933ff03154270697bad86c011ebbe
Попов Денис <dpopov@stream.ru>
2020-07-06 12:39:43 +0500

    d

--- ListVsDict/Program.cs
+++ ListVsDict/Program.cs
@@ -8,24 +8,8 @@ namespace ListVsDict
 	{
 		static void Main(string[] args)
 		{
+			//you MUST run it under release build
 			var summary = BenchmarkRunner.Run<ListVsDict>();
-
-			//var s = new ListVsDict();
-			//s.Setup();
-			//s.FillAndSeekDict();
-
-			//var dict = new Dictionary<int, KindaComplexObject>(3);
-			//var dict2 = new HashSet<KindaComplexObject>(3);
-			//var data = new KindaComplexObject[3];
-			//
-			//for (var i = 0; i < 3; i++)
-			//	data[i] = KindaComplexObject.Create(i);
-			//
-			//for (int i = 0; i < 3; i++)
-			//	dict.Add(i, data[i]);
-			//
-			//for (int i = 0; i < 3; i++)
-			//	dict2.Add(data[i]);
 		}
 	}
 }
